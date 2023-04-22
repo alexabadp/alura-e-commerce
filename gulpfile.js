a3a4ccd1c94a4180bc87a58222e5095848c26b46
alexabadp <aalex.abad96@gmail.com>
2023-04-21 22:16:33 -0500

    configuracion gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
 const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
 
 function css(done) {
   src("src/scss/**/*.scss") //Identifica el archivo
     .pipe(sass()) //compilarlo
+    .pipe(plumber()) //para evitar detener el gulp ante algun error
     .pipe(dest("build/css")); //guarda en la memoria
 
   done();
 }
 
 function dev(done) {
-  watch("src/scss/**/*.scss", css);
+  watch("src/scss/**/*.scss", css); // Observar los cambios y actualizarlos
 
   done();
 }
