686ee6227a2fc17976bec855e12ec418d3e4ff95
alexabadp <aalex.abad96@gmail.com>
2023-04-22 03:17:46 -0500

    seccion banner estilos ok

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const { src, dest, watch, parallel } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
-
 // Imagenes
 const cache = require("gulp-cache");
 const imagemin = require("gulp-imagemin");
